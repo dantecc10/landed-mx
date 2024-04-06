0aad863c1921f581f6a55a9fd3252d9e02ccfe00
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-06 00:32:42 -0600

    specify host

--- php scripts/connection.php
+++ php scripts/connection.php
@@ -1,4 +1,4 @@
 <?php
 include_once "credentials.php";
 $data = generatePasskey('sql');
-$connection = new mysqli("localhost", $data[0], $data[1], $data[2]);
+$connection = new mysqli("landed.castelancarpinteyro.com", $data[0], $data[1], $data[2]);
