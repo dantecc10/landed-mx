62ad4e4d586f3f16da8aabbaa96b26e3a96fc18d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-04-06 00:13:17 -0600

    start backend

---
+++ php scripts/login.php
@@ -0,0 +1,12 @@
+<?php
+session_destroy();
+session_start();
+include_once "connection.php";
+
+if ($connection->connect_error) {
+    die("La conexión a la base de datos falló: " . $connection->connect_error);
+} else {
+    echo ("Conexión establecida");
+}
+
+// Cerrar la conexión a la base de datos
